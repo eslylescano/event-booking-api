311266004512222ec4570590a73fd301179afb2f
Esly Lescano <eslylescano@gmail.com>
2024-02-17 22:17:51 +0000

    Add tests for endpoint

--- main.go
+++ main.go
@@ -32,5 +32,6 @@ func createEvent(context *gin.Context) {
 	}
 	event.ID = 1
 	event.UserdId = 1
+	event.Save()
 	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
 }
