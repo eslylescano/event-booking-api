404b7cc67cae4c66ee0db998ac2f24e633c20c1c
Esly Lescano <eslylescano@gmail.com>
2024-02-18 19:42:13 +0000

    Add GetAllEvents implementation, add missing column in table

--- models/event.go
+++ models/event.go
@@ -15,8 +15,6 @@ type Event struct {
 	UserdId     int
 }
 
-var events []Event = []Event{}
-
 func (e *Event) Save() error {
 	query := `
 	INSERT INTO events(name, description, location, dateTime, user_id)
@@ -35,6 +33,25 @@ func (e *Event) Save() error {
 
 }
 
-func GetAllEvents() []Event {
-	return events
+func GetAllEvents() ([]Event, error) {
+	query := "SELECT * FROM events"
+	rows, err := db.DB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var events []Event = []Event{}
+
+	for rows.Next() {
+		var event Event
+		err := rows.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserdId)
+
+		if err != nil {
+			return nil, err
+		}
+		events = append(events, event)
+	}
+
+	return events, nil
 }
