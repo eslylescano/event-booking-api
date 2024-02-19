ba5166c60849d407ef9bfcc9f59eb0e65782fdb5
Esly Lescano <eslylescano@gmail.com>
2024-02-19 23:34:16 +0000

    Add hash password

--- models/user.go
+++ models/user.go
@@ -1,6 +1,9 @@
 package models
 
-import "example.com/mod/db"
+import (
+	"example.com/mod/db"
+	"example.com/mod/utils"
+)
 
 type User struct {
 	ID       int64
@@ -18,7 +21,13 @@ func (u User) Save() error {
 
 	defer smt.Close()
 
-	result, err := smt.Exec(u.Email, u.Password)
+	hashedPassword, err := utils.HashPassword(u.Password)
+
+	if err != nil {
+		return err
+	}
+
+	result, err := smt.Exec(u.Email, hashedPassword)
 
 	if err != nil {
 		return err
